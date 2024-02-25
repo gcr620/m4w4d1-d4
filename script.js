99b8efa0d0311946690aa64cf03dba32e5dd43d0
gcr620 <148818562+gcr620@users.noreply.github.com>
2024-02-25 14:27:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ let val = document.querySelector("form");
 // per il form
 let form = document.querySelectorAll(".form-control")
 console.log(form);
+
+let n = document.getElementById("nameval");
+let b = document.getElementById("brandval");
+let p = document.getElementById("priceval");
+let d = document.getElementById("descval");
+let i = document.getElementById("imgval");
+
+let tempMod;
+
 // query string
 let params = new URLSearchParams(window.location.search);
 let query = params.get("q");
@@ -26,12 +35,13 @@ if (!window.location.search) {
     // let query = params.get("q");
     fetch(endPoint + query, {
         headers: {
-        "Authorization": apiKey
+        "Authorization": apiKey,
+        "Content-type": "application/json;charset=UTF-8"
         }
         })
     .then(res => res.json())
     .then(raw => infoCreation(raw))
-    .catch(error => console.log("errore info"))
+    .catch(error => allert("errore info"))
 
 }
 
@@ -40,14 +50,15 @@ async function getprod() {
     try {
         let res = await fetch(endPoint, {
         headers: {
-        "Authorization": apiKey
+        "Authorization": apiKey,
+        "Content-type": "application/json;charset=UTF-8"
         }
         })
     let raw = await res.json();
     console.log(raw);
     createList(raw)    
     } catch (error) {
-        console.log("ERRORE FRA");
+        alert("nell'ottenere i dati dal server");
     }
 }
 // creazione lista prodotti
@@ -60,8 +71,10 @@ function createList(raw) {
         card.classList.add("card");
         let img = document.createElement("img");
         img.classList.add("card-img-top");
+        img.setAttribute('alt', 'IMAGE NOT FOUND')
         let cardBody = document.createElement("div");
         cardBody.classList.add("card-body");
+        cardBody.setAttribute('id', 'C-' + prod._id)
         let title = document.createElement("h5");
         title.classList.add("card-title");
         let text = document.createElement("p");
@@ -74,6 +87,7 @@ function createList(raw) {
         // cambia la proprietà del bottone in base alla pagina
         if (back[0].innerText === "Back office") {
             btn.setAttribute('onclick', 'modify()')
+            btn.setAttribute('id', prod._id)
             btn.classList.add("desc" ,"btn", "btn-danger", "px-5");
             btn.innerText = "Modifica";  
         } else {
@@ -120,7 +134,7 @@ function infoCreation(raw) {
 }
 
 // aggiungere prodotti
-
+// verifica se i campi sono compilati
 function check() {
     let valCount = 0;
     form.forEach(inp => {
@@ -138,24 +152,20 @@ function check() {
         addProd()
     }
 }
-
+// crea tutti i prodotti
 async function addProd() {
     // 0 name , 1 brand , 2 price , 3 description , 4 img
     try {
         // payload
-        let n = form[0].value;
-        let b = form[1].value;
-        let p = form[2].value;
-        let d = form[3].value;
-        let i = form[4].value;
 
         console.log(n);
         console.log(b);
         console.log(p);
         console.log(d);
         console.log(i);
+
         // body
-        let Mybody = { name: n, description: d, brand: b, imageUrl: i, price: p};
+        let Mybody = { name: n.value, description: d.value, brand: b.value, imageUrl: i.value, price: p.value};
         // post per aggiungere prodotto
         let post = await fetch(endPoint, {
             method: "POST", body: JSON.stringify(Mybody),
@@ -167,7 +177,103 @@ async function addProd() {
         let res = await post.json()
         console.log(res);  
     } catch (error) {
-        console.log("errore nel post");
+        alert("errore nel post");
     }  
 }
 // modifica dei prodotti
+// fa partire la modifica e copia i dati nel form
+async function modify() {
+    try {
+        tempMod = event.target.id;
+        console.log(tempMod + " è l'id");
+        let allBtn = document.querySelectorAll("a.btn");
+        console.log(allBtn);
+        console.log("sopra sono i bottoni");
+
+        allBtn.forEach(bt => {
+            console.log(bt.id);
+            if (bt.id !== "subProd") {
+                bt.classList.add("disabled")
+            } else {
+                bt.innerText = "Done";
+                bt.setAttribute('onclick', 'verify()')
+            }
+    })
+        let get = await fetch(endPoint + tempMod, {
+            headers: {
+                "Authorization": apiKey,
+                "Content-type": "application/json;charset=UTF-8"
+            }
+        })
+        let raw = await get.json();
+        console.log("risultato mod get:");
+        console.log(raw);
+        n.value = raw.name;
+        b.value = raw.brand;
+        p.value = raw.price;
+        d.value = raw.description;
+        i.value = raw.imageUrl;
+        alert("Return back to top to modify")   
+    } catch (error) {
+        alert("errore nel tentare di modificare")
+    }  
+}
+// modifiche finite
+// verifica se tutti i campi sono compilati
+function verify() {
+    let valCount = 0;
+    form.forEach(inp => {
+        // console.log(valCount);
+        if (inp.validity.valid) {
+            valCount ++;
+            console.log("nuovo cont: " + valCount);
+            val.classList.add("was-validated");
+        } else {
+            val.classList.add("was-validated");
+        }
+    })
+    if (valCount === 5) {
+        alert("Tutti i campi sono validi");
+        putProd()
+   } else {
+        alert("Riempire tutti i campi")
+   }
+}
+
+async function putProd() {
+    try {
+        // let tempD = event.target.id;
+        console.log(tempMod + " è l'id");
+        let allBtn = document.querySelectorAll("a.btn");
+        console.log(allBtn);
+        console.log("sopra sono i bottoni");
+        // let cardBody = document.getElementById("C-" + tempD)
+        // console.log(cardBody.children);
+
+        let myBody = { "name": n.value, "description": d.value, "brand": b.value, "imageUrl": i.value, "price": p.value};
+        console.log(myBody);
+        let put = await fetch(endPoint + tempMod, {
+            method: "PUT", body: JSON.stringify(myBody),
+            headers: {
+            "Authorization": apiKey,
+            "Content-type": "application/json;charset=UTF-8"
+            }
+            })
+        let raw = await put.json();
+        console.log("questo è il put:");
+        console.log(raw);    
+        allBtn.forEach(bt => {
+            console.log(bt.id);
+            if (bt.id !== "subProd") {
+                bt.classList.remove("disabled")
+                bt.setAttribute('onclick', 'modify()')
+                bt.innerText = "Modify";
+            } else {
+                bt.classList.remove("disabled")
+            }
+        })
+        alert("Prodotto modificato! Ricarica la pagina per vedere");
+    } catch (error) {
+            alert("errore nel put");
+    }
+}
